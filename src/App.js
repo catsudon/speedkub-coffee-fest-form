97442853a78db429665b34d343a1797b39dd2cab
mark48853 <sathanalao@hotmail.com>
2022-06-25 16:50:02 +0700

    I want to sleep

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
     liff.shareTargetPicker([
       {
         "type": "text",
-        "text": "ref = "+ref
+        "text": "ref = "
       },
       {
         "type": "flex",
